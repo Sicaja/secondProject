e09b60fb4ff4de25bfc2d9722b62889b86366101
Julio Sicajá <jsicaja@bi.com.gt>
2023-06-06 11:54:28 -0600

    standalone

---
+++ src/app/components/components.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-components',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './components.component.html',
+  styleUrls: ['./components.component.scss']
+})
+export class ComponentsComponent {
+
+}
