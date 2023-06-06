e4b0187c19345f33f8bd1769c45b72c1f8eb47b0
Julio Sicajá <jsicaja@bi.com.gt>
2023-06-06 12:26:53 -0600

    add translate module

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { TranslateModule } from '@ngx-translate/core';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      TranslateModule.forRoot()
+    ),
+  ],
+}).catch((err) => console.error(err));
